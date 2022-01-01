48ecbe4381bcb39e93a6397956f82a66ebaa0b67
Edgard Vilo <evilo@hostyweb.cl>
2022-01-01 12:57:22 -0300

    Clase 235: Estructura y diseño del HomeScreen

--- src/components/notes/NoteScreen.js
+++ src/components/notes/NoteScreen.js
@@ -6,6 +6,29 @@ export const NoteScreen = () => {
             
             <NotesAppBar />
 
+            <div className="notes__content">
+                
+                <input 
+                    type="text"
+                    placeholder="Some awesome title"
+                    className="notes__title-input"
+                    autoComplete="off"
+                />
+
+                <textarea
+                    placeholder="What happened today"
+                    className="notes__textarea">
+
+                </textarea>
+
+                <div className="notes__image">
+                    <img 
+                        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSi9Au4kI1MDK89jE28pV7EPgudelWfF5OIkw&usqp=CAU" 
+                        alt="imagen" 
+                    />
+                </div>
+            </div>
+
         </div>
     )
 }
