ae5ec1b2d41bf56b9af70ad5c797062fee5d80b1
Edgard Vilo <evilo@hostyweb.cl>
2022-01-13 14:53:51 -0300

    Clase 250: Manejo de errores del formulario

--- src/components/auth/RegisterScreen.js
+++ src/components/auth/RegisterScreen.js
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom"
 import { useForm } from "../../hooks/useForm";
+import  validator from 'validator';
 
 export const RegisterScreen = () => {
 
@@ -14,11 +15,25 @@ export const RegisterScreen = () => {
 
     const handleRegister = (e) => {
         e.preventDefault();
-        console.log(name, email, password, password2);
+        
+        if(isFormValid()) {
+            console.log('formulario correcto');
+        }
     }
 
     const isFormValid = () => {
+        if( name.trim().length === 0 ) {
+            console.log('Name is required');
+            return false;
+        } else if ( !validator.isEmail( email ) ) {
+            console.log('Email is not valid');
+            return false;
+        } else if (password !== password2 || password.length <= 5) {
+            console.log('Password should be at least 6 characters and match each other');
+            return false;
+        }
         
+        return true;
     }
 
     return (
@@ -26,6 +41,11 @@ export const RegisterScreen = () => {
             <h3 className="auth__title">Register</h3>
 
             <form onSubmit={handleRegister}>
+
+                <div className="auth__alert-error">
+                    Hola Mundo
+                </div>
+
                 <input 
                     type="text"
                     placeholder="Name"
