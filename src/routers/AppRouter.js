054ceb75e0fb7b313ef9a8089ab3f85d4b864aa7
Edgard Vilo <evilo@hostyweb.cl>
2022-01-13 19:05:43 -0300

    clase 256: Mantener el estado de la autenticación al recargar

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,12 +1,29 @@
+import { getAuth, onAuthStateChanged } from 'firebase/auth'
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
 import {
     BrowserRouter as Router,
     Redirect,
     Route, 
     Switch } from 'react-router-dom'
+import { login } from '../actions/auth'
 import { JournalScreen } from '../components/journal/JournalScreen'
 import { AuthRouter } from './AuthRouter'
 
 export const AppRouter = () => {
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        const auth = getAuth();
+        onAuthStateChanged(auth, (user) => {
+            
+            if (user?.uid) {
+                dispatch( login(user.uid, user.displayName) );
+            }
+
+        })
+    }, [dispatch]);
+
     return (
         <Router>
             <div>
