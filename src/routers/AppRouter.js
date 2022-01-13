c9c8bf6c639bbfceaabd65246a67f54fe5096294
Edgard Vilo <evilo@hostyweb.cl>
2022-01-13 19:14:01 -0300

    Clase 257: Mostrar un loading global en la aplicación

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,11 +1,12 @@
 import { getAuth, onAuthStateChanged } from 'firebase/auth'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useDispatch } from 'react-redux'
 import {
     BrowserRouter as Router,
     Redirect,
     Route, 
     Switch } from 'react-router-dom'
+
 import { login } from '../actions/auth'
 import { JournalScreen } from '../components/journal/JournalScreen'
 import { AuthRouter } from './AuthRouter'
@@ -13,16 +14,29 @@ import { AuthRouter } from './AuthRouter'
 export const AppRouter = () => {
     const dispatch = useDispatch();
 
+    const [checking, setChecking] = useState(true);
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
     useEffect(() => {
         const auth = getAuth();
         onAuthStateChanged(auth, (user) => {
             
             if (user?.uid) {
                 dispatch( login(user.uid, user.displayName) );
+                setIsLoggedIn(true);
+            } else {
+                setIsLoggedIn(false);
             }
+            setChecking(false);
 
         })
-    }, [dispatch]);
+    }, [dispatch, setChecking, setIsLoggedIn]);
+
+    if (checking) {
+        return (
+            <h1>Espere</h1>
+        );
+    }
 
     return (
         <Router>
