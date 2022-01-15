61e0de67fbef7689e6f68670c4fb5e3d68e59bae
Edgard Vilo <evilo@hostyweb.cl>
2022-01-15 15:35:28 -0300

    Clase 272: Activar unanota para s edició

--- src/components/notes/NoteScreen.js
+++ src/components/notes/NoteScreen.js
@@ -1,6 +1,14 @@
+import { useSelector } from "react-redux";
+import { useForm } from "../../hooks/useForm";
 import { NotesAppBar } from "./NotesAppBar"
 
 export const NoteScreen = () => {
+
+    const { active: note } = useSelector(state => state.notes);
+    const [formValues, handleInputChange] = useForm(note);
+
+    const { title, body } = formValues;
+
     return (
         <div className="notes__main-content">
             
@@ -13,20 +21,29 @@ export const NoteScreen = () => {
                     placeholder="Some awesome title"
                     className="notes__title-input"
                     autoComplete="off"
+                    value={title}
+                    onChange={handleInputChange}
                 />
 
                 <textarea
                     placeholder="What happened today"
-                    className="notes__textarea">
-
+                    className="notes__textarea"
+                    value={body}
+                    onChange={handleInputChange}
+                >
                 </textarea>
 
-                <div className="notes__image">
-                    <img 
-                        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSi9Au4kI1MDK89jE28pV7EPgudelWfF5OIkw&usqp=CAU" 
-                        alt="imagen" 
-                    />
-                </div>
+                {
+                    note.url 
+                    && (
+                        <div className="notes__image">
+                            <img 
+                                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSi9Au4kI1MDK89jE28pV7EPgudelWfF5OIkw&usqp=CAU" 
+                                alt="imagen" 
+                            />
+                        </div>
+                    )
+                }
             </div>
 
         </div>
