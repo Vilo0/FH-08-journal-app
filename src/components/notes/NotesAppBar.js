78a24b9398c380bafcc685917dc970d9660b23ac
Edgard Vilo <evilo@hostyweb.cl>
2022-01-15 16:49:35 -0300

    Clase 274: Actualizar la nota actual

--- src/components/notes/NotesAppBar.js
+++ src/components/notes/NotesAppBar.js
@@ -1,4 +1,14 @@
+import { useDispatch, useSelector } from "react-redux"
+import { startSaveNote } from "../../actions/notes";
+
 export const NotesAppBar = () => {
+    const dispatch = useDispatch();
+    const { active } = useSelector(state => state.notes);
+
+    const handleSave = () => {
+        dispatch(startSaveNote(active));
+    }
+
     return (
         <div className="notes__appbar">
             <span>01 de enero 2021</span>
@@ -8,7 +18,8 @@ export const NotesAppBar = () => {
                     Picture
                 </button>
 
-                <button className="btn">
+                <button className="btn"
+                    onClick={handleSave}>
                     Save
                 </button>
             </div>
